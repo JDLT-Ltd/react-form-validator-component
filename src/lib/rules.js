88d2fb3346a2a295d950e42eace7ea0b4aaaea11
Fabian Feldberg <feldbergf@gmail.com>
2018-06-29 17:28:19 +0100

    more rules and new build

--- src/lib/rules.js
+++ src/lib/rules.js
@@ -1,6 +1,8 @@
 const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 const emailArrayRegex = /^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-Z]{2,5}){1,25}(,[ ]{0,1}([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-Z]{2,5}){1,25})*$/
 const nameRegex = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/
+const phoneNumberRegex = /^[+]?(\d{1,3})\s?(\d{10})$/
+const postcodeRegex = /^(([A-Za-z0-9]{2,4})\s?([A-Za-z0-9]{1,5})?)?$/
 
 const isRequired = {
   validator: data => {
@@ -32,6 +34,22 @@ const isEmailArray = {
     return false
   },
   error: 'Please provide a valid email address, or several emails comma delimited'
+}
+
+const isPhoneNumber = {
+  validator: data => {
+    if (data.match(phoneNumberRegex)) return true
+    return false
+  },
+  error: 'Please provide a valid UK phone number'
+}
+
+const isPostcode = {
+  validator: data => {
+    if (data.match(postcodeRegex)) return true
+    return false
+  },
+  error: 'Please provide a valid UK postcode'
 }
 
 export { isRequired, isEmail, isEmailArray, isFullName }
