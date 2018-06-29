e2451707820ddf1921a43f4a84a03d67a1dfcb68
Fabian Feldberg <feldbergf@gmail.com>
2018-06-29 17:25:24 +0100

    added more rules, changed repo[

--- src/lib/rules.js
+++ src/lib/rules.js
@@ -1,3 +1,7 @@
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+const emailArrayRegex = /^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-Z]{2,5}){1,25}(,[ ]{0,1}([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-Z]{2,5}){1,25})*$/
+const nameRegex = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/
+
 const isRequired = {
   validator: data => {
     if (data) return true
@@ -6,14 +10,28 @@ const isRequired = {
   error: 'Please provide a value'
 }
 
-const emailArrayRegex = /^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-Z]{2,5}){1,25}(,[ ]{0,1}([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-Z]{2,5}){1,25})*$/
+const isFullName = {
+  validator: data => {
+    if (data.match(nameRegex)) return true
+    return false
+  },
+  error: 'Please provide a full name'
+}
+
+const isEmail = {
+  validator: data => {
+    if (data.match(emailRegex)) return true
+    return false
+  },
+  error: 'Please provide a valid email address'
+}
 
 const isEmailArray = {
   validator: data => {
     if (data.match(emailArrayRegex)) return true
     return false
   },
-  error: 'Please provide a valid email address'
+  error: 'Please provide a valid email address, or several emails comma delimited'
 }
 
-export { isRequired, isEmailArray }
+export { isRequired, isEmail, isEmailArray, isFullName }
