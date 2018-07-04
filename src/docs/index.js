052bf5854d25df5f540d9a610846ca9a330809f5
Fabian Feldberg <feldbergf@gmail.com>
2018-07-04 23:24:19 +0100

    added a lot of stuff to docs/index

--- src/docs/index.js
+++ src/docs/index.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import reactDOM from 'react-dom'
-import { Form, Header, Label, Input, Container } from 'semantic-ui-react'
+import { Form, Header, Label, Input, Container, Button } from 'semantic-ui-react'
 
 import { Validator } from '../lib/index'
 
-// const inputMap = {
-//   input: props => <Input {...props} />,
-//   textArea: props => <TextArea {...props} />
-// }
+//this is a custom rule
 const isRequired = {
   validator: data => {
     if (data) return true
@@ -19,32 +16,34 @@ class App extends React.Component {
   constructor(props) {
     super(props)
 
-    this.state = {
-      fields: {
-        emailAddresses: {
-          name: 'emailAddresses',
-          rules: ['isEmailArray', isRequired],
-          label: 'Email addresses'
-        },
-        something: {
-          name: 'something',
-          label: 'Something'
-        }
-      }
+    this.state = {}
+  }
+
+  fields = {
+    emailAddresses: {
+      name: 'emailAddresses',
+      rules: ['isEmailArray', 'isRequired'],
+      label: 'Email addresses'
+    },
+    something: {
+      name: 'something',
+      rules: ['isRequired'],
+      label: 'Something'
     }
   }
 
   render() {
     return (
       <Container>
-        <Header as="h1">Hello</Header>
-        <Validator fields={this.state.fields} parent={this} validateOnLoad>
-          {({ isValid, fields, onChange, errors }) => {
+        <Header as="h1">Examples for using RFVC</Header>
+        <Header as="h2">Basic Validation</Header>
+        <Validator fields={this.fields} parent={this}>
+          {({ isFormValid, onChange, errors }) => {
             return (
               <Form>
                 <Form.Field>
                   <label>Your Emails</label>
-                  <Input name="emailAddresses" onChange={onChange} value="test" />
+                  <Input name="emailAddresses" onChange={onChange} />
                   {errors.emailAddresses.map((error, i) => {
                     return <Label key={i}>{error}</Label>
                   })}
@@ -56,15 +55,134 @@ class App extends React.Component {
                     return <Label key={i}>{error}</Label>
                   })}
                 </Form.Field>
-                {fields.map(field => {
-                  return (
-                    <Form.Field key={field.value.label}>
-                      <label>{field.value.label}</label>
-                      <Input name={field.key} onChange={onChange} />
-                    </Form.Field>
-                  )
-                })}
-                {isValid && <span>Form is valid</span>}
+                {<span>Form is {isFormValid ? 'valid' : 'not valid'}</span>}
+                <hr />
+                <Button disabled={!isFormValid}>Test</Button>
+              </Form>
+            )
+          }}
+        </Validator>
+        <Header as={'h2'}>Basic Validation using fields to map (and no semantic-ui)</Header>
+        <Validator fields={this.fields} parent={this}>
+          {({ isFormValid, fields, onChange, errors }) => {
+            return (
+              <Form>
+                {fields &&
+                  fields.map((input, i) => {
+                    console.log(input)
+                    return (
+                      <div key={i}>
+                        <label>{input.value.label}</label>
+                        <input name={input.value.name} onChange={onChange} />
+                        {errors[input.value.name].map((error, i) => {
+                          return <label key={i}>{error}</label>
+                        })}
+                      </div>
+                    )
+                  })}
+                {<span>Form is {isFormValid ? 'valid' : 'not valid'}</span>}
+                <hr />
+                <button disabled={!isFormValid}>Test</button>
+              </Form>
+            )
+          }}
+        </Validator>
+        <Header as="h2">Validation using isFieldValid</Header>
+        <Validator fields={this.fields} parent={this}>
+          {({ isFormValid, isFieldValid, onChange, errors }) => {
+            return (
+              <Form>
+                <Form.Field>
+                  <label>Your Emails</label>
+                  <Input name="emailAddresses" onChange={onChange} />
+                  {errors.emailAddresses.map((error, i) => {
+                    return <Label key={i}>{error}</Label>
+                  })}
+                </Form.Field>
+                <Button onClick={() => alert('sure is')} disabled={!isFieldValid.emailAddresses}>
+                  Thats an Email!
+                </Button>
+                <Form.Field>
+                  <label>Something</label>
+                  <Input name="something" onChange={onChange} />
+                  {errors.something.map((error, i) => {
+                    return <Label key={i}>{error}</Label>
+                  })}
+                </Form.Field>
+                <Button onClick={() => alert('is it?')} disabled={!isFieldValid.something}>
+                  Its not nothing
+                </Button>
+                {<span>Form is {isFormValid ? 'valid' : 'not valid'}</span>}
+                <hr />
+                <button disabled={!isFormValid}>Test</button>
+              </Form>
+            )
+          }}
+        </Validator>
+        <Header as="h2">Validation using validateOnLoad and isFieldValid</Header>
+        <Validator fields={this.fields} parent={this} validateOnLoad>
+          {({ isFormValid, isFieldValid, onChange, errors }) => {
+            return (
+              <Form>
+                <Form.Field>
+                  <label>Your Emails</label>
+                  <Input name="emailAddresses" onChange={onChange} value={'I_DONT_WORK'} />
+                  {errors.emailAddresses.map((error, i) => {
+                    return <Label key={i}>{error}</Label>
+                  })}
+                </Form.Field>
+                <Button onClick={() => alert('sure is')} disabled={!isFieldValid.emailAddresses}>
+                  Thats an Email!
+                </Button>
+                <Form.Field>
+                  <label>Something</label>
+                  <Input name="something" onChange={onChange} defaultValue={''} />
+                  {errors.something.map((error, i) => {
+                    return <Label key={i}>{error}</Label>
+                  })}
+                </Form.Field>
+                <Button onClick={() => alert('is it?')} disabled={!isFieldValid.something}>
+                  Its not nothing
+                </Button>
+                {<span>Form is {isFormValid ? 'valid' : 'not valid'}</span>}
+                <hr />
+                <button disabled={!isFormValid}>Test</button>
+              </Form>
+            )
+          }}
+        </Validator>
+        <Header as="h2">Validation using a custom onValidate handler</Header>
+        <Validator
+          fields={this.fields}
+          onValidate={(fieldName, fieldValue) => this.setState({ hidden: fieldValue })}
+          validateOnLoad
+        >
+          {({ isFormValid, isFieldValid, onChange, errors }) => {
+            return (
+              <Form>
+                <Form.Field>
+                  <label>Your Emails</label>
+                  <Input name="emailAddresses" onChange={onChange} />
+                  {errors.emailAddresses.map((error, i) => {
+                    return <Label key={i}>{error}</Label>
+                  })}
+                </Form.Field>
+                <Button onClick={() => alert('sure is')} disabled={!isFieldValid.emailAddresses}>
+                  Thats an Email!
+                </Button>
+                <Form.Field>
+                  <label>Something</label>
+                  <Input name="something" onChange={onChange} />
+                  {errors.something.map((error, i) => {
+                    return <Label key={i}>{error}</Label>
+                  })}
+                </Form.Field>
+                <Button onClick={() => alert('is it?')} disabled={!isFieldValid.something}>
+                  Its not nothing
+                </Button>
+                {<span>Form is {isFormValid ? 'valid' : 'not valid'}</span>}
+                <hr />
+                <button disabled={!isFormValid}>Test</button>
               </Form>
             )
           }}
