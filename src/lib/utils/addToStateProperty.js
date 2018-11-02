40065abfcf0e9c2897a1e6668604271e2a5ba390
Fabian Feldberg <monsagri@gmail.com>
2018-11-02 15:24:53 +0000

    lets see

--- src/lib/utils/addToStateProperty.js
+++ src/lib/utils/addToStateProperty.js
@@ -1,6 +1,12 @@
 export default (target, value, context) => {
-  console.log('args in addtoState', target, value, context)
-  context.setState({
-    [target]: Object.assign(context.state[target], value)
-  })
+  context.setState(
+    {
+      [target]: Object.assign(context.state[target], value)
+    },
+    () => {
+      if (target === 'validation')
+        console.log('addToStateProp targeted validation, rechecking isFormValid with: ', context.state.validation)
+      context.setState({ isFormValid: Object.values(context.state.validation).every(field => field === true) })
+    }
+  )
 }
